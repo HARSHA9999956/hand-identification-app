3bf788392c99ed2aa889edc19b894ddedf6d9e50
harsha <74890373+HARSHA9999956@users.noreply.github.com>
2021-04-14 20:12:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+var prediction_1=""
+var prediction_2=""
+Webcam.set({
+    Width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90,
+});
+camera-document.getElementById("camera");
+
+Webcam.attach('#camera')
+
+function takesnapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+console.log('ml5version',ml5.version)
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/FT7W0NzZ0/.modeljson',modelLoaded);
+
+function modelLoaded(){
+console.log('modelLoaded!');
+}
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data_1="The first prediction is"+prediction_1;
+    speak_data_2="The second prediction is"+prediction_2;
+    var utterthis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+    synth.speak(utterthis);
+}
